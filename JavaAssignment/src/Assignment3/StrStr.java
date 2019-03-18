0927508ca614d9886f85bc7b3af2c951cfcce935
Naushad Sindhwani <sindhwani.naushad@gmail.com>
2019-03-17 21:51:54 -0500

    Added description

--- JavaAssignment/src/Assignment3/StrStr.java
+++ JavaAssignment/src/Assignment3/StrStr.java
@@ -2,6 +2,24 @@ package Assignment3;
 
 public class StrStr {
 
+	/**
+	 * Implement strStr().
+
+ strstr - locate a substring ( needle ) in a string ( haystack ). 
+Try not to use standard library string functions for this question.
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ NOTE: Good clarification questions:
+What should be the return value if the needle is empty?
+What if both haystack and needle are empty?
+For the purpose of this problem, assume that the return value should be -1 in both cases.
+	 * @param args
+	 */
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
